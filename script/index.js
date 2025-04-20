14a0a5a4bac5174bfb5f301e86d0e8b4c6c5eec3
unknown <mdeyasinbhuiya.info@gmail.com>
2025-04-20 17:36:24 +0600

    add nav var and nav button

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
-console.log("this is connectted");
+function loadCategories() {
+  fetch(" https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => dataList(data.categories));
+}
+const dataList = (categories) => {
+  const categorisNav = document.getElementById("categorisNav");
+  for (let cat of categories) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    `;
+    categorisNav.appendChild(categoryDiv);
+  }
+};
+loadCategories();
